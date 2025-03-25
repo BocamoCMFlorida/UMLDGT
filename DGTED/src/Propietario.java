b3166d6403bcca701098445f8e6842478d519504
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-03-25 07:32:32 +0100

    Constructor Propietario

--- DGTED/src/Propietario.java
+++ DGTED/src/Propietario.java
@@ -3,4 +3,11 @@ public class Propietario {
     private String Nombre;
     private String Apellidos;
     private int Puntos;
+
+    public Propietario(String DNI, String nombre, String apellidos, int puntos) {
+        this.DNI = DNI;
+        Nombre = nombre;
+        Apellidos = apellidos;
+        Puntos = puntos;
+    }
 }
