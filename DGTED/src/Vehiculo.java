69c96d93a61a885621b6fafb5208863dddbd0693
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-03-25 07:45:14 +0100

    Getter y Setter Vehiculo

--- DGTED/src/Vehiculo.java
+++ DGTED/src/Vehiculo.java
@@ -12,4 +12,44 @@ public class Vehiculo {
         this.KM = KM;
         FechaFabricacion = fechaFabricacion;
     }
+
+    public String getMatricula() {
+        return Matricula;
+    }
+
+    public void setMatricula(String matricula) {
+        Matricula = matricula;
+    }
+
+    public String getMarca() {
+        return Marca;
+    }
+
+    public void setMarca(String marca) {
+        Marca = marca;
+    }
+
+    public String getPropietario() {
+        return Propietario;
+    }
+
+    public void setPropietario(String propietario) {
+        Propietario = propietario;
+    }
+
+    public Double getKM() {
+        return KM;
+    }
+
+    public void setKM(Double KM) {
+        this.KM = KM;
+    }
+
+    public int getFechaFabricacion() {
+        return FechaFabricacion;
+    }
+
+    public void setFechaFabricacion(int fechaFabricacion) {
+        FechaFabricacion = fechaFabricacion;
+    }
 }
