df1889754d1e2547cd901e3d5961fb28dfbdd5b8
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-03-25 07:44:33 +0100

    Constructor Vehiculo

--- DGTED/src/Vehiculo.java
+++ DGTED/src/Vehiculo.java
@@ -4,4 +4,12 @@ public class Vehiculo {
     private String Propietario;
     private Double KM;
     private int FechaFabricacion;
+
+    public Vehiculo(String matricula, String marca, String propietario, Double KM, int fechaFabricacion) {
+        Matricula = matricula;
+        Marca = marca;
+        Propietario = propietario;
+        this.KM = KM;
+        FechaFabricacion = fechaFabricacion;
+    }
 }
