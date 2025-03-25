6c4e26dd52389dac59761ffc658d94cc5d80d7f7
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-03-25 07:27:17 +0100

    Getters y Setters

--- DGTED/src/Oficina.java
+++ DGTED/src/Oficina.java
@@ -2,4 +2,28 @@ public class Oficina {
     private String Ciudad;
     private String Direccion;
     private int Cpostal;
+
+    public String getCiudad() {
+        return Ciudad;
+    }
+
+    public void setCiudad(String ciudad) {
+        Ciudad = ciudad;
+    }
+
+    public String getDireccion() {
+        return Direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        Direccion = direccion;
+    }
+
+    public int getCpostal() {
+        return Cpostal;
+    }
+
+    public void setCpostal(int cpostal) {
+        Cpostal = cpostal;
+    }
 }
