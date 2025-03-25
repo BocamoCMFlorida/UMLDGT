1e8536d284f2f8b41ddad98fe47a846b9035f2b6
BocamoCMFlorida <bocamo@alumnatflorida.es>
2025-03-25 07:57:19 +0100

    Clase Moto

--- DGTED/src/Moto.java
+++ DGTED/src/Moto.java
@@ -1,2 +1,8 @@
-public class Moto {
+class Moto extends Vehiculo {
+    private int potencia;
+
+    public Moto(String matricula, String marca, String propietario, Double KM, String fechaFabricacion, int potencia) {
+        super(matricula, marca, propietario, KM, fechaFabricacion);
+        this.potencia = potencia;
+    }
 }
